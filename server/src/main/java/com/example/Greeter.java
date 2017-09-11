1c73ce37af8c60cf15ba38ce67735ba45c3665ba
Sudhir Panda <sudhirpanda@Sudhirs-MacBook-Pro.local>
2017-09-10 22:06:35 -0700

    init commit

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,10 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
+  /**
+  * @author SK
+  *
+  */
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
